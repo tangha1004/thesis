4ef0615476f22ecd58591c2b4706208b704e6bff
Ha Tang <121566444+t1hachane@users.noreply.github.com>
2025-08-27 23:23:04 +0700

    add load_model_dict

--- models_VAE/utils.py
+++ models_VAE/utils.py
@@ -1,8 +1,23 @@
 import torch
 import os
+import numpy as np
+import torch.nn.functional as F
+
+cuda = True if torch.cuda.is_available() else False
 
 def save_model_dict(folder, model_dict):
     if not os.path.exists(folder):
         os.makedirs(folder)
     for module in model_dict:
         torch.save(model_dict[module].state_dict(), os.path.join(folder, module+".pt"))
+
+def load_model_dict(folder, model_dict):
+    for module in model_dict:
+        if os.path.exists(os.path.join(folder, module+".pt")):
+#            print("Module {:} loaded!".format(module))
+            model_dict[module].load_state_dict(torch.load(os.path.join(folder, module+".pt"), map_location="cuda:{:}".format(torch.cuda.current_device())))
+        else:
+            print("WARNING: Module {:} from model_dict is not loaded!".format(module))
+        if cuda:
+            model_dict[module].cuda()    
+    return model_dict
